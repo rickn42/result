7b7f01065ddcfa04379165ab328022510a3807ba
rickn42 <rickn42morty@gmail.com>
2017-10-16 21:02:14 +0900

    add methods: IsDone(), Err()

--- result.go
+++ result.go
@@ -6,7 +6,10 @@ import "sync"
 type Result interface {
 	Set(val interface{})
 	Done() <-chan struct{}
+	IsDone() bool
+	Wait()
 	Value() (interface{}, error)
+	Err() error
 }
 
 func NewResult() Result {
@@ -34,11 +37,21 @@ func (r *result) Done() <-chan struct{} {
 	return r.C
 }
 
+func (r *result) IsDone() (ok bool) {
+	select {
+	case <-r.C:
+		ok = true
+	default:
+	}
+
+	return ok
+}
+
 func (r *result) Wait() {
 	<-r.C
 }
 
-func (r *result) Value() (interface{}, error) {
+func (r *result) Value() (val interface{}, err error) {
 	<-r.C
 
 	r.mu.Lock()
@@ -48,5 +61,21 @@ func (r *result) Value() (interface{}, error) {
 	if ok {
 		return nil, err
 	}
-	return r.val, nil
+
+	val = r.val
+	return val, nil
+}
+
+func (r *result) Err() (err error) {
+	<-r.C
+
+	r.mu.Lock()
+	err, ok := r.val.(error)
+	r.mu.Unlock()
+
+	if ok {
+		return err
+	}
+
+	return nil
 }
